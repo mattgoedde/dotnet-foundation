57f2d89a25df2401dc03cb5797cebc0993babfbc
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2022-08-22 09:54:41 -0400

    format phone numbers

--- DotnetExtensions/StringExtensions.cs
+++ DotnetExtensions/StringExtensions.cs
@@ -7,7 +7,14 @@ namespace DotnetExtensions
     public static partial class StringExtensions
     {
         // Phone Number extensions
+        private static Func<string, string> RemoveNonNumeric = x => Regex.Replace(x, "[^0-9]", string.Empty);
+        private static Func<string, string> FormatAreaCode = x => x.Insert(0, "(").Insert(3, ")");
+        private static Func<string, string> InsertDash = x => x.Insert(7, "-");
         public static Func<string, bool> IsNumeric = x => new Regex("[0-9]").IsMatch(x);
+
+        public static string FormatPhoneNumber(this string phoneNumber)
+        => phoneNumber.Map(RemoveNonNumeric).Map(FormatAreaCode).Map(InsertDash);
+
         public static bool ValidatePhoneNumber(this string phoneNumber) => phoneNumber.Validate(IsNumeric);
 
         // Email extensions
