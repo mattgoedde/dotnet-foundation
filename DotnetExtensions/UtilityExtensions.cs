61ef770fe48fff7dbb2e381ba7d2972d17b98b85
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2022-08-22 08:22:47 -0400

    add utility extensions

---
+++ DotnetExtensions/UtilityExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace DotnetExtensions
+{
+    public static partial class UtilityExtensions
+    {
+        public static bool Validate<TInput>((this TInput item, params Func<TInput, bool>[] predicates) => predicates.All(x => x(item)));
+    }
+}
