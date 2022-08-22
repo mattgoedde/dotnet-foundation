5baa0689c74fec73f6fd78880633b1f8735ac93a
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2022-08-22 08:23:50 -0400

    fix parathesis

--- DotnetExtensions/UtilityExtensions.cs
+++ DotnetExtensions/UtilityExtensions.cs
@@ -5,6 +5,6 @@ namespace DotnetExtensions
 {
     public static partial class UtilityExtensions
     {
-        public static bool Validate<TInput>((this TInput item, params Func<TInput, bool>[] predicates) => predicates.All(x => x(item)));
+        public static bool Validate<TInput>(this TInput item, params Func<TInput, bool>[] predicates) => predicates.All(x => x(item));
     }
 }
