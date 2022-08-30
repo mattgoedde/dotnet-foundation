fab79bfa7a9a68cd18b1f4b40500acb997caba5c
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2022-08-30 09:11:16 -0400

    add test project

---
+++ Extensions.Test/Utilities.Test.cs
@@ -0,0 +1,16 @@
+namespace Extensions.Test;
+
+public class UtilitiesTests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
