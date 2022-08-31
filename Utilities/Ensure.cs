5e7ee22ff1a4d1e4ae206a32c3e13980b4127a02
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2022-08-31 15:45:20 -0400

    utilities project with ensure class

---
+++ Utilities/Ensure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Utilities;
+public static class Ensure
+{
+
+    public static bool ItIsNotNull<T>(T value)
+        => Ensure.ItIsTrue(value is not null);
+
+    public static bool ItIsEmpty<T>(IEnumerable<T> collection)
+        => Ensure.ItIsTrue(collection.Any());
+
+    public static bool ItIsNotEmpty<T>(IEnumerable<T> collection)
+        => Ensure.ItIsTrue(!collection.Any());
+
+    public static bool ItIsTrue(bool condition)
+    {
+        if (!condition)
+        {
+            throw new ArgumentException(null, nameof(condition));
+        }
+        return true;
+    }
+
+    public static bool ItIsTrue(Func<bool> expression)
+    {
+        if (!expression())
+        {
+            throw new ArgumentException(null, nameof(expression));
+        }
+        return true;
+    }
+}
